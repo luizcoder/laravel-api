0d0633fa2e69a07e478feb153794b757c204d61c
José Luiz Martins Oliveira <martinsmanager@gmail.com>
2015-09-30 17:51:45 -0300

    Alterado url base para testes Adicionado .travis.yml ao projeto

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      *
      * @var string
      */
-    protected $baseUrl = 'http://api.dev';
+    protected $baseUrl = 'http://localhost';
 
    /**
      * Default preparation for each test
